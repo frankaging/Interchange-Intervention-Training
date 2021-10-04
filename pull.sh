ec0aa703eb8bdba79755561e6e8ae0b5dc8ab7f3
frankaging <zxw0424@gmail.com>
2021-10-04 12:13:21 -0700

    update bash for causal distill

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
 git pull origin main
 cd examples/ReaSCAN-Causal; git pull origin main; cd ../../
 cd examples/MQNLICounterfactualTraining; git pull origin main; cd ../..
-
+cd examples/Causal-Distill; git pull origin main; cd ../..
